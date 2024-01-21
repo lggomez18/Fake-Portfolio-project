61bff333999a61ecb0f11ecc35beb3c39f367d9c
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-01-21 17:00:14 -0600

    accessibility button functionality
    in progress and review

---
+++ src/script.js
@@ -0,0 +1,10 @@
+
+function changeTheme(){
+    let body=document.querySelector("body");
+
+    body.classList.toggle("dark");
+}
+
+let themeButton = document.querySelector (".theme-button");
+themeButton.addEventListener ('click', changeTheme);
+ 
