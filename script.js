27f8394b4a4c7f967e458a597c9ce6b2b3108e94
lggomez18 <137099040+lggomez18@users.noreply.github.com>
2024-01-23 17:07:07 -0600

    Accessibility font-size in progress
    font size change for accessibility purposes in progress.

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function changeTheme(){
 let themeButton = document.querySelector (".theme-button");
 themeButton.addEventListener ('click', changeTheme);
  
+let buttons =document.querySelector ('.buttons');
+let btn = buttons.querySelectorAll('.btn');
+for (var i = 0; i <btn.length; i++){
+    btn[i].addEventListener('click',function(){
+        let current = document.getElementsByClassName ('.active');
+        current[0].className=current[0].className.replace("active", "");
+        this.className+= " active";
+    })
+}
